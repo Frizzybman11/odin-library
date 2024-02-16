00d4bb75352e76870a35edcd64ebc1bedee739be
Frizzybman11 <Frizzybman11@aol.com>
2024-02-16 17:08:30 -0500

    Able to add book to library

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const myLibrary = [];
 const dialog = document.getElementById("form-dialog");
 const showButton = document.getElementById("new-book");
-const closeButton = document.getElementById("add-book");
+const closeButton = document.getElementById("close-form");
+const confirmButton = document.getElementById("confirm-form")
+let book = {};
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -9,13 +11,19 @@ function Book(title, author, pages, read){
     this.pages = pages;
     this.read = read;
     this.info = function(){
-        info = title + " by " + author + ", " + pages + ", " + read
+        info = [title, author, pages, read]
         return info
     }
 }
 
-function addBookToLibrary(){
-    //
+function addBookToLibrary(book){
+    myLibrary.push(book);
+}
+
+function displayBooks(myLibrary){
+    for (i = 0; i < myLibrary.length; i++){
+        
+    }
 }
 
 showButton.addEventListener("click", () => {
@@ -25,3 +33,14 @@ showButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
     dialog.close();
 });
+
+confirmButton.addEventListener("click", () => {
+    let title = document.getElementById("title").value
+    let author = document.getElementById("author").value
+    let pages = document.getElementById("pages").value
+    let read = document.querySelector('input[name="read"]:checked').value
+    let book_num = myLibrary.length + 1
+    book_num = book_num.toString()
+    book[book_num] = new Book(title, author, pages, read)
+    addBookToLibrary(book[book_num])
+})
