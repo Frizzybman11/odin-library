41a925e5fc243a094a5419358a8a665b49395ef0
Frizzybman11 <Frizzybman11@aol.com>
2024-02-20 14:35:01 -0500

    Add book to library

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const dialog = document.getElementById("form-dialog");
 const showButton = document.getElementById("new-book");
 const closeButton = document.getElementById("close-form");
 const confirmButton = document.getElementById("confirm-form")
-let book = {};
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -39,8 +38,6 @@ confirmButton.addEventListener("click", () => {
     let author = document.getElementById("author").value
     let pages = document.getElementById("pages").value
     let read = document.querySelector('input[name="read"]:checked').value
-    let book_num = myLibrary.length + 1
-    book_num = book_num.toString()
-    book[book_num] = new Book(title, author, pages, read)
-    addBookToLibrary(book[book_num])
+    let book = new Book(title, author, pages, read)
+    addBookToLibrary(book)
 })
